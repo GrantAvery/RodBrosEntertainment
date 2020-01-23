243905d01e5aff5dedb25a042c9243b85627f0ac
Grant Avery <grantavery@icloud.com>
2020-01-23 12:53:12 -0500

    Setup .NET Core MVC base

---
+++ RodBrosEntertainment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RodBrosEntertainment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
